86aa35ae70c835fa5aabf5b1792edce01886b59a
unknown <58028608+applabsllc@users.noreply.github.com>
2022-05-02 01:05:44 -0500

    added buttons
    added buttons, background, layout

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import type {Node} from 'react';
+import React , { useState} from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -11,44 +10,77 @@ import {
 } from 'react-native';
 
 
-const handleClick = () => {
-	
-	console.log("click");
-}
-
 const App = ()  => {
-  
-console.log("OK");
+
+  const [flashActive, setFlashActive] = useState(false);
+
+  const labelButton1 = "Turn Flashlight " + (flashActive?"Off":"On");
+  const labelButton2 = "Strobe";
+  const labelButton3 = "SOS";
+
+  const operateFlashlight = () => { //function to turn on an off simple flashlight
+    const currentActive = flashActive;
+    console.log("currentActive:", currentActive);
+    setFlashActive(!currentActive);
+    
+  }
+
+  const operateStrobe = () => { //function to turn flash on/off on interval
+    console.log("click operateStrobe");
+
+  }
+
+  const operateSOS = () => { // call sequenced operateStrobe()
+    console.log("click operateSOS");
+    
+  }
 
   return (
-    <Button
-	  onPress={handleClick}
-	  title="Press Me"
-	/>
+    <View style={styles.container}>
+      <View style={styles.topContainer} />
+      <View style={styles.bottomContainer} >
+        <View style={styles.buttonWrapper}>
+          <Button
+            
+            onPress={operateFlashlight}
+            title={labelButton1}
+          />
+        </View>
+        <View style={styles.buttonWrapper}>
+        <Button
+        style={styles.buttonWrapper}
+          onPress={operateStrobe}
+          title={labelButton2}
+        />
+        </View>
+        <View style={styles.buttonWrapper}>
+        <Button
+          style={styles.buttonWrapper}
+          onPress={operateSOS}
+          title={labelButton3}
+        />
+        </View>
+      </View>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
 	 container: {
+		flex: 1,
+    flexDirection: "column",
+  },
+  topContainer: {
     flex: 1,
-
+    backgroundColor: "#9ec5f7",
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  bottomContainer: {
+    flex: 1.7,
+    backgroundColor: "#9ec5f7",
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+  buttonWrapper: {
+    margin: 10,
+  }
 });
 
 export default App;
--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# flashApp
- Applabs FlashApp
+# AppLabs Flashlight App
+
+AppLabs Flashlight App is a mobile app that showcases the use of a mobile device's flash functionality, accessing the native flashlight API to create a flash, sequence of flashes, or an SOS flash for emergencies.
+
+The app is free to use and has no special requirements. The only permissions necessary are access to the devices flashlight module, in some devices this may be the same permission required for the camera due to the device having one module that shares both hardware devices.
+
+The app is built using React Native and react-native-torch package. It is intended only for educational purposes.
+
+## Installation
+
+Use the package manager NPM to install the required dependencies:
+
+```bash
+npm install
+```
+In order to build Android version (apk) you must configure the Gradle properties and assign the proper SDK API level and MIN SDK level you wish to build for.
+
+To build the project and test on a virtual (emulated) device, open a compatible device from Android's AVD Manager and run the following command:
+
+```bash
+react-native run-android
+```
+
+
+## Contributing
+
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
